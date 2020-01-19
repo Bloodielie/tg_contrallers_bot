a51237170622cffb340ca56565455e0fc4b7ef9e
Bloodielie <49410211+Bloodielie@users.noreply.github.com>
2020-01-19 21:37:54 +0300

    Add files via upload

--- scenes/kontroler.py
+++ scenes/kontroler.py
@@ -4,6 +4,7 @@ from aiogram import types
 from config import SAVE_DEFAULT_TABLE, DEFAULT_TABLE, FONT_PNG, MESSAGE_KEYBOARD
 from utils.pil import img_busstop
 from utils.utils import text_display
+from utils.mixin import mixin_create_kontroler
 
 
 class Scene(BaseScene):
@@ -11,22 +12,24 @@ class Scene(BaseScene):
         data = await self.manager.get(self.model, user_id=message.from_user.id)
         if message.text == MESSAGE_KEYBOARD['kontroler_keyb_clear_stop']:
             temporary_data = await self.getter.get_data_bus(type='clean', time=data.time, sort=data.sort)
-            if data.display == 'Фото':
-                img_busstop(name_png=SAVE_DEFAULT_TABLE, _dict=temporary_data, cordinates_x=(60, 650, 1250), cordinate_y=45, y_step=92,
-                            color=(34, 34, 34), font=FONT_PNG, name_png_first=DEFAULT_TABLE)
-                await self.bot.send_photo(message.from_user.id, types.InputFile('png/table.gif'), reply_markup=self.keyboard.kontroler_keyboard())
-            else:
-                text = text_display(temporary_data)
-                await self.bot.send_message(message.from_user.id, text, reply_markup=self.keyboard.kontroler_keyboard())
+            await mixin_create_kontroler(self.bot, data, temporary_data, self.keyboard, message, FONT_PNG, DEFAULT_TABLE, SAVE_DEFAULT_TABLE)
+            #if data.display == 'Фото':
+            #    img_busstop(name_png=SAVE_DEFAULT_TABLE, _dict=temporary_data, cordinates_x=(60, 650, 1250), cordinate_y=45, y_step=92,
+            #                color=(34, 34, 34), font=FONT_PNG, name_png_first=DEFAULT_TABLE)
+            #    await self.bot.send_photo(message.from_user.id, types.InputFile('png/table.gif'), reply_markup=self.keyboard.kontroler_keyboard())
+            #else:
+            #    text = text_display(temporary_data)
+            #    await self.bot.send_message(message.from_user.id, text, reply_markup=self.keyboard.kontroler_keyboard())
         elif message.text == MESSAGE_KEYBOARD['kontroler_keyb_dirty_stop']:
             temporary_data = await self.getter.get_data_bus(type='dirty', time=data.time, sort=data.sort)
-            if data.display == 'Фото':
-                img_busstop(name_png=SAVE_DEFAULT_TABLE, _dict=temporary_data, cordinates_x=(60, 650, 1250), cordinate_y=45, y_step=92,
-                            color=(34, 34, 34), font=FONT_PNG, name_png_first=DEFAULT_TABLE)
-                await self.bot.send_photo(message.from_user.id, types.InputFile('png/table.gif'), reply_markup=self.keyboard.kontroler_keyboard())
-            else:
-                text = text_display(temporary_data)
-                await self.bot.send_message(message.from_user.id, text, reply_markup=self.keyboard.start_keyboard())
+            await mixin_create_kontroler(self.bot, data, temporary_data, self.keyboard, message, FONT_PNG, DEFAULT_TABLE, SAVE_DEFAULT_TABLE)
+            #if data.display == 'Фото':
+            #    img_busstop(name_png=SAVE_DEFAULT_TABLE, _dict=temporary_data, cordinates_x=(60, 650, 1250), cordinate_y=45, y_step=92,
+            #                color=(34, 34, 34), font=FONT_PNG, name_png_first=DEFAULT_TABLE)
+            #    await self.bot.send_photo(message.from_user.id, types.InputFile('png/table.gif'), reply_markup=self.keyboard.kontroler_keyboard())
+            #else:
+            #    text = text_display(temporary_data)
+            #    await self.bot.send_message(message.from_user.id, text, reply_markup=self.keyboard.kontroler_keyboard())
         elif message.text == MESSAGE_KEYBOARD['kontroler_keyb_bus']:
             data.scens = 'kontroler_bus'
             await self.manager.update(data)
